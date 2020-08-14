feaabc27756fccc98a43997a4c69e5fa916d3a0e
Viktoriia Strelnikova <v.strelnikova91@gmail.com>
2020-08-15 00:41:51 +0300

    добавляет решение 6 задач Модуля-1

---
+++ js/task-03.js
@@ -0,0 +1,43 @@
+'use strict';
+const userPassword = 'jqueryismyjam';
+
+const ADMIN_PASSWORD = 'jqueryismyjam';
+
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_IS_ALLOWED = 'Добро пожаловать!';
+const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!';
+let message;
+
+if (ADMIN_PASSWORD === 'jqueryismyjam') {
+    message = ACCESS_IS_ALLOWED;
+} else {
+    message = ACCESS_DENIED;
+}
+
+if (userPassword === 'jqueryismyjam') {
+    message = ACCESS_IS_ALLOWED;
+} else {
+    message = ACCESS_DENIED;
+}
+
+if (userPassword === null) {
+    message = CANCELED_BY_USER;
+}
+
+if (userPassword === '123') {
+    message = ACCESS_DENIED;
+}
+
+console.log(message);
+
+//если userPassword равно 'jqueryismyjam' 
+// то значение message будет равно
+// 'Добро пожаловать!'
+
+//если userPassword равно  null 
+// то значение message будет равно  
+// 'Отменено пользователем!'
+
+//если userPassword равно '123' 
+// то значение message будет равно
+// 'Доступ запрещен, неверный пароль!'
---
+++ js/task-04.js
@@ -0,0 +1,33 @@
+'use strict';
+const orderPieces = 4;
+
+const credits = 23580;
+const pricePerDroid = 3000;
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_DENIED = 'Недостаточно средств на счету!';
+
+let totalPrice = orderPieces * pricePerDroid; 
+let balanceCredit = credits - totalPrice; 
+let message;
+
+if (orderPieces === null) {
+    message = CANCELED_BY_USER;
+} else if (totalPrice > credits) {
+    message = ACCESS_DENIED;
+} else if (totalPrice <= credits) {
+    message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
+}
+
+console.log(message);
+
+//если orderPieces равно 4
+// то значение message будет равно
+// 'Вы купили 4 дроидов, на счету осталось 11580 кредитов'
+
+//если orderPieces равно null
+// то значение message будет равно
+// 'Отменено пользователем!'
+
+//если orderPieces равно 10
+// то значение message будет равно
+// 'Недостаточно средств на счету!'
